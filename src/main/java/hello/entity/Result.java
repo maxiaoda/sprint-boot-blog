a874fe74b4bc533d70eaffa437298858a21f50b1
maxiaoda <631677979@qq.com>
2020-04-03 20:29:16 +0800

    spring-boot-blog 完成登录模块 优化重复代码，练习静态工厂设计模式

--- src/main/java/hello/entity/Result.java
+++ src/main/java/hello/entity/Result.java
@@ -1,20 +1,32 @@
 package hello.entity;
 
 public class Result {
-    String status;
-    String msg;
-    boolean isLogin;
-    Object data;
+    private String status;
+    private String msg;
+    private boolean isLogin;
+    private Object data;
 
-    public Result(String status, String msg, boolean isLogin) {
+    private Result(String status, String msg, boolean isLogin) {
         this(status, msg, isLogin, null);
     }
 
-    public Result(String status, String msg, boolean isLogin, Object data) {
+    private Result(String status, String msg, boolean isLogin, Object data) {
         this.status = status;
         this.msg = msg;
         this.isLogin = isLogin;
         this.data = data;
+    }
+
+    public static Result failMsg(String msg) {
+        return new Result("fail", msg, false);
+    }
+
+    public static Result statusIsOkAndLoginIsFalse(String msg) {
+        return new Result("ok", msg, false);
+    }
+
+    public static Result successMsg(String msg, Object loggerInUser) {
+        return new Result("ok", msg, false, loggerInUser);
     }
 
     public String getStatus() {
